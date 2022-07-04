af0cfebc32248a1abec1e9f0d40ee415a4e01ee0
Duc Hieu <hieund276@gmail.com>
2022-07-04 23:38:43 +0700

    add contact, supplier

---
+++ src/main/resources/static/assets/js/contact.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function() {
+const btnEditList = document.querySelectorAll('.table .btn-primary');
+btnEditList.forEach(button => {
+	button.addEventListener('click', function(event) {
+	event.preventDefault();
+	let href = this.getAttribute('href');
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open("GET", href);
+	xmlHttp.send();
+	xmlHttp.onreadystatechange = function(){
+		let jsonObject = JSON.parse(xmlHttp.responseText);
+		document.getElementById("txtIdEdit").value = jsonObject.id;
+		document.getElementById("txtRemarksEdit").value = jsonObject.remarks;
+		document.getElementById("txtLastnameEdit").value = jsonObject.lastname;
+		document.getElementById("txtFirstnameEdit").value = jsonObject.firstname;
+		document.getElementById("txtPhoneEdit").value = jsonObject.phone;
+		document.getElementById("txtMobileEdit").value = jsonObject.mobile;
+		document.getElementById("txtEmailEdit").value = jsonObject.email;
+	};
+	
+	var myModal = new bootstrap.Modal(document.getElementById('editModal'));
+	myModal.show();
+});
+});
+
+const btnDelList = document.querySelectorAll('.table .btn-danger')
+btnDelList.forEach(button => {
+	button.addEventListener('click', function(event) {
+	event.preventDefault();
+	let href1 = this.getAttribute('href');
+	document.getElementById('confirmDelButton').href = href1;
+	var myModal = new bootstrap.Modal(document.getElementById('deleteModal'));
+	myModal.show();
+	});
+});		
+
+
+});
+
+
